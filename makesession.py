e023270921b1d5c2b1a3e219bb0b11bea3a5022c
myxasa <109124619+yasavwxyz@users.noreply.github.com>
2022-09-05 14:46:23 +0700

    Add files via upload

---
+++ makesession.py
@@ -0,0 +1,7 @@
+from pyrogram import client
+
+
+API_KEY = int(input("Enter API KEY: "))
+API_HASH = input("Enter API HASH: ")
+with Client(":memory:", api_id=API_KEY, api_hash=API_HASH) as app:
+    print(app.export_session_string())
