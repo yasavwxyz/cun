21e16a550cb75e9b25c08343230c9e12d868715c
myxasa <109124619+yasavwxyz@users.noreply.github.com>
2022-09-09 11:03:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 
-API_HASH = os.getenv("API_HASH")
-API_ID = int(os.getenv("API_ID"))
-MONGO_URI = os.getenv("MONGO_URI")
+API_HASH = os.getenv("12557329")
+API_ID = int(os.getenv("333f70291772d39d8bd4c246e4feb3fc"))
+MONGO_URI = os.getenv("mongodb+srv://nekomaru:nekomaru24@cluster0.uv6mq1k.mongodb.net/?retryWrites=true&w=majority")
 SESSION = os.getenv("SESSION")
 PREFIX = os.getenv("PREFIX")
-LOG_CHAT = int(os.getenv("LOG_CHAT"))
-HEROKU_API = os.getenv("HEROKU_API")
-HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+LOG_CHAT = int(os.getenv("botlogf"))
+HEROKU_API = os.getenv("9e517a08-f592-4779-ae45-55cdf212be72")
+HEROKU_APP_NAME = os.getenv("superncun")
